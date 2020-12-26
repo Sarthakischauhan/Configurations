a7a329662d770529013e18c34b26ed216d18e15c
sarthakischauhan18 <sarthakchauhann@gmail.com>
2020-12-26 23:18:10 +0530

    practice file

---
+++ practice.sh
@@ -0,0 +1,61 @@
+#! /c/Program Files/Git/bin/bash 
+
+
+
+#IF ELSE
+
+#NAME="Jia"
+
+#if [ "$NAME" == "Sarthak" ]
+#then
+#    echo ""		
+#    echo "Hello sarthak"
+
+#elif [ "$NAME" == "Jia" ]
+#then 
+#    echo "Welcome Jia"
+
+#else 
+#    echo "Get lost"
+#fi 
+
+
+
+#COMPARISON
+
+#NUM1=3
+#read -p "Enter the second number : " NUM2
+#echo 
+#if [ "$NUM1" -gt "$NUM2" ]
+#then
+#   echo "$NUM2 is smaller than $NUM1 " 
+#fi
+
+
+
+# WORKING WITH FILE
+
+#FILE="text.txt"
+
+#if [ -f $FILE ]
+#then 
+#   echo "This is a file"
+#else 
+#   echo "This is not a file"
+#fi 
+
+
+#LOGIN SYSTEM
+
+#read -p "Enter username" USERNAME
+#echo ""
+#read -sp "Enter password" PASS
+#echo ""
+#
+#if [ $PASS == "SAR" ]
+#then
+#   echo "Welcome user"
+#fi
+
+
+
