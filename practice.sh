eb8f908abf35fbe67de915b809b7d197890b8c5d
sarthakischauhan18 <sarthakchauhann@gmail.com>
2020-12-29 12:50:30 +0530

    Did some stuff

--- practice.sh
+++ practice.sh
@@ -78,13 +78,78 @@
 #esac		
 
 
+#echo -n "Are you 18 or not Y/N "
+#read ANSWER
+#echo ""
+#case $ANSWER in  
+#	[yY] | [yY][eE][sS])
+#		echo "You are allowed to drink alcohol"
+#		;;
+#	[nN] | [nN][oO])
+#		echo "You are not allowed,we are sorry"
+#		;;
+#	*)
+#		echo "Unknown"
+#		;;
+#esac	
+
+#FOR LOOPS
+
+NAMES="SARTHAK"
+
+#for NAME in $NAMES
+#do
+#	echo "Hello $NAME"
+#done
+
+#for i in {0..10..5}
+#do
+#	echo "Number $i"
+#done
+
+BOOKS=('In Search of Lost Time' 'Don Quixote' 'Ulysses' 'The Great Gatsby')
+#echo "${BOOKS[@]}"
+#for BOOK in "${BOOKS[@]}"
+#do
+#	echo "Book : $BOOK"
+#done
 
 
-		
+
+#C type loops 
+
+
+#for (( i=5 ; i<=10 ; i++))
+#do
+#	echo "$i"
+#done	
+
+
+#BREAK AND CONTINUE STATEMENT
+
+#for NAME in Helios Spanky Bonky
+#do
+#	echo $NAME
+#	if [ "$NAME" == "Helios" ]
+#	then
+#		echo "Found his dog , Breaking the loop folks"
+#		break
+#	else  
+#		echo "Not his dog"
+#	fi
+#done
 
 
 
+#TAKING FILES
+
+FILES=*.py
+
+#for FILE in $FILES
+#do
+#	rm $FILE
+#done	
 
 
-	
+
 
